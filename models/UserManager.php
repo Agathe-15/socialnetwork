d7a8bcd7c6be87423a271f75c7741a6ba84ce622
Agathe Delieutraz <agathedelieutraz@MacBook-Pro-de-Agathe.local>
2024-05-27 15:07:32 +0200

    Simple login/logout system

--- models/UserManager.php
+++ models/UserManager.php
@@ -14,3 +14,21 @@ function GetAllUsers()
   $response = $PDO->query("SELECT * FROM user ORDER BY nickname ASC");
   return $response->fetchAll();
 }
+
+function GetUserIdFromUserAndPassword($username, $password)
+{
+  // Préparer la requête SQL pour récupérer l'utilisateur avec le nom d'utilisateur donné
+  global $PDO;
+  $stmt = $PDO->prepare("SELECT id, password FROM user WHERE username = :username");
+  $stmt->execute(['username' => $username]);
+
+  // Récupérer l'utilisateur
+  $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+  // Vérifier si l'utilisateur existe et si le mot de passe est correct
+  if ($user && password_verify($password, $user['password'])) {
+    return $user['id'];
+  } else {
+    return -1;
+  }
+}
